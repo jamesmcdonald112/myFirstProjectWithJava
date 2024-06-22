f91d2f6e17977a990443c17ee023a1114dd35ee7
James McDonald <jamesmcdonald112@yahoo.ie>
2024-06-22 13:27:47 +0100

    Finished part 2/3. Fixed format issue by removing extra blank lines

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+h
+8My First Project with Java/task/src/calculator/Main.java,e/2/e21c268eef39eafa0d572af75f0b0c0a1b3c62c2
--- My First Project with Java/task/src/calculator/Main.java
+++ My First Project with Java/task/src/calculator/Main.java
@@ -1,12 +1,19 @@
 package calculator;
 
+import com.sun.source.tree.Tree;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) {
         Map<String, Double> prices = priceList();
         printPrices(prices);
+        System.out.println(" ");
+        Map<String, Double> incomeList = incomeList();
+        printEarnedAmount(incomeList);
+        printIncome(calculateIncome(incomeList));
 
     }
 
@@ -18,7 +25,7 @@ public class Main {
     }
 
     private  static Map<String, Double> priceList() {
-        Map<String, Double> prices = new HashMap<>();
+        Map<String, Double> prices = new TreeMap<>();
 
         prices.put("Bubblegum", 2d);
         prices.put("Toffee", 0.2);
@@ -28,5 +35,37 @@ public class Main {
         prices.put("Pancake", 3.2);
 
         return prices;
+    }
+
+    public static void printEarnedAmount(Map<String, Double> incomeList) {
+        System.out.println("Earned amount:");
+        for(Map.Entry<String, Double> entry : incomeList.entrySet()) {
+            System.out.println(entry.getKey() + ": $" + entry.getValue());
+        }
+    }
+
+    private static Map<String, Double> incomeList() {
+        Map<String, Double> incomeList = new TreeMap<>();
+
+        incomeList.put("Bubblegum", 202d);
+        incomeList.put("Toffee", 118d);
+        incomeList.put("Ice cream", 2250d);
+        incomeList.put("Milk chocolate", 1680d);
+        incomeList.put("Doughnut", 1075d);
+        incomeList.put("Pancake", 80d);
+
+        return incomeList;
+    }
+
+    private static double calculateIncome(Map<String, Double> incomeList) {
+        double sum = 0;
+        for(Map.Entry<String, Double> entry : incomeList.entrySet()) {
+            sum += entry.getValue();
+        }
+        return sum;
+    }
+
+    private static void printIncome(double sum) {
+        System.out.println("Income: " + sum);
     }
 }
